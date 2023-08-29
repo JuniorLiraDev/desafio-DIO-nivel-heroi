d67d017523fb967f971cf59cf1d5b9cc47b4d057
JuniorLiraDev <juniorlirati@gmail.com>
2023-08-29 15:14:32 -0300

    Desafio resolvido usando if/else

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Desafio Classificador de nível de Herói")
+
+let nome = "Super Herói"
+let xp = 1.000
+
+if(xp<1.000){
+  console.log("O Herói de nome " + nome + " está no nível de Ferro")
+}else if (xp<=2.000){
+  console.log("O Herói de nome " + nome + " está no nível de Bronze")
+}else if (xp<=5.000){
+  console.log("O Herói de nome " + nome + " está no nível de Prata")
+}else if (xp<=7.000){
+  console.log("O Herói de nome " + nome + " está no nível de Ouro")
+}else if (xp<=8.000){
+  console.log("O Herói de nome " + nome + " está no nível de Platina")
+}else if (xp<=9.000){
+  console.log("O Herói de nome " + nome + " está no nível de Ascendente")
+}else if (xp<=10.000){
+  console.log("O Herói de nome " + nome + " está no nível de Imortal")
+}else{
+  console.log(Radiante)
+}
